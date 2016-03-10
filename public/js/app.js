6418b61036bb134eff68fcb91b7025f487dcc472
melvinr <melreijnoudt@gmail.com>
2016-03-10 22:12:07 +0100

    Implemented routie and refactored website to Single page webapp

--- public/js/app.js
+++ public/js/app.js
@@ -1,32 +1,85 @@
-(function () {
-    'use strict';
+var ll = ll || {};
+'use strict';
 
-    /**
-     * Invoked when the page is ready.
-     *
-     * @param  {Function} fn
-     * @return {void}
-     */
-    function ready(fn) {
-        if (document.readyState !== 'loading') {
-            fn();
-        } else {
-            document.addEventListener('DOMContentLoaded', fn);
-        }
+document.addEventListener('DOMContentLoaded', function () {
+    ll.launcher.init();
+});
+
+ll.launcher = (function () {
+    var init = function () {
+        ll.routes.init();
+    };
+
+    return {
+        init: init
     }
 
-    /**
-     * Set the classes on the appearence page.
-     *
-     * @return {void}
-     */
-    function appearance() {
+})();
+
+ll.routes = (function () {
+    var routeCheck = function () {
+        if (location.hash === undefined || location.hash === '') {
+            location.hash = '#/';
+        }
+    };
+
+    var route = function () {
+        routie({
+            '/': function () {
+                ll.page.home();
+            },
+            '/appearance/:uuid': ll.page.appearance
+        });
+    };
+
+    var init = function () {
+        routeCheck();
+        route();
+    }
+
+    return {
+        init: init
+    }
+})();
+
+ll.page = (function () {
+
+    var home = function () {
+        var url = '/api/feed';
+        //pass the correct api url to the promise
+       ll.get.data(url)
+            //if this is done, respond by adding the response from the request a specific HTML element
+            .then(response => {
+                var target = document.getElementById("target");
+                target.innerHTML = response;
+           
+            })
+           //If there is an error, show this in the console
+           .catch(e => {
+                console.error(e);
+            });
+    };
+
+    var appearance = function (uuid) {
+        var url = 'api/appearance/' + uuid;
+
+        ll.get.data(url)
+            .then(response => {
+                var target = document.getElementById("target");
+                target.innerHTML = response;
+                product();
+            }).catch(e => {
+                console.error(e);
+            });
+    };
+
+    var product = function (data) {
         var firstProduct = document.querySelector('.product');
         var firstIndicator = document.querySelector(
             '.product-indicator[data-uuid="' + firstProduct.getAttribute('data-uuid') + '"]'
         );
         var indicators = document.querySelectorAll('.product-indicator');
-
+        
         firstProduct.classList.add('product-active');
         firstIndicator.classList.add('product-indicator-active');
 
@@ -49,12 +102,41 @@
                 event.currentTarget.classList.add('product-indicator-active');
             });
         });
+    };
+
+    return {
+        home: home,
+        appearance: appearance
     }
 
-    ready(function () {
-        if (/appearance/.test(window.location.href)) {
-            appearance();
-        }
-    });
-}());
+})();
 
+ll.get = (function() {
+
+    function data(url) {
+        // return a new Promise object
+        return new Promise((resolve, reject) => {
+            var request = new XMLHttpRequest();
+            //open a get request, pass it the correct url, and set async to true
+            request.open('GET', url, true);
+
+            request.onload = function () {
+                //ony if request is done
+                if (request.status == 200) {
+                    // send text that belongs to the request api
+                    resolve(request.responseText);
+                } else {
+                    // if the request is rejected, show an error
+                    reject(new Error('request failed!'));
+                }
+            };
+            //send the request, if you do not declare this, the request will never be done
+            request.send();
+        });
+    };
+
+    return {
+        data: data
+    }
+
+})();
